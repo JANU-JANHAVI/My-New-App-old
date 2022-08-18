04fc6d8efc29e3d5e1ca196ddab5dbd99409358a
JANU-JANHAVI <84670122+JANU-JANHAVI@users.noreply.github.com>
2022-08-18 10:34:11 +0530

    Update details.component.ts

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,6 +4,9 @@ import { HttpClient } from '@angular/common/http';
 import { appConstant } from '../app.constant';
 import { environment } from '../environments/environment';
 import { Product } from '../product/product';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormControlName } from '@angular/forms';
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -13,16 +16,38 @@ export class DetailsComponent implements OnInit {
 
   product: Product | any = '';
   id: number | string = '';
+  productForm: FormGroup;
+
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    private formBuilder: FormBuilder
+
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((data) => {
       this.getDetails((data as any).id);
     });
+    this.initialize();
+
+  }
+  initialize() {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.productForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['',Validators.required],
+      category: ['', Validators.required],
+      price: ['', Validators.required],
+    });
+  }
+
+  onSubmit(){
+
   }
 
   getDetails(id: number) {
